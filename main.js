6605e1c734eb6d1f8d4de5ccdbee187bdb10e8b4
Gleison Batista <gleisonbs@gmail.com>
2019-08-11 11:18:09 -0300

    complete refactor

--- main.js
+++ main.js
@@ -1,56 +1,37 @@
-const makeTable = () => {
+const getDistanceBetween = (firstWord, secondWord) => {
+    const distanceTable = makeTable(firstWord, secondWord)
+    return distanceTable[secondWord.length][firstWord.length]
+}
+
+const makeTable = (firstWord, secondWord) => {
+    let distanceTable = [ makeFirstRow(firstWord) ]
+    for (let row = 1; row < secondWord.length + 1; row++)
+        distanceTable.push(makeRow(distanceTable[row - 1], row, firstWord, secondWord))
+
+    return distanceTable
+}
+
+const makeFirstRow = (firstWord) => [...Array(firstWord.length + 1).keys()]
+
+const makeRow = (previousRow, currentRowPos, firstWord, secondWord) => {
+    currentRow = [currentRowPos]
+    const totalCols = firstWord.length + 1
+    for (let colPos = 1; colPos < totalCols; colPos++)
+        currentRow.push(getMinima(currentRow, previousRow, currentRowPos, colPos, firstWord, secondWord))
+
+    return currentRow
+}
+
+const getMinima = (currentRow, previousRow, rowPos, colPos, firstWord, secondWord) => {
+    const isSameLetter = firstWord[colPos - 1] === secondWord[rowPos - 1]
+    return Math.min(currentRow[colPos - 1] + 1, previousRow[colPos] + 1, previousRow[colPos - 1] + (isSameLetter ? 0 : 2))
+}
+
+const main = () => {
     const firstWord = process.argv[2]
     const secondWord = process.argv[3]
-
-    console.log("making table for " + firstWord + " and " + secondWord)
-
-    let distanceTable = makeFirstCol(secondWord)
-    distanceTable[0] = makeFirstRow(firstWord)
-    fillTable(distanceTable, firstWord, secondWord)
-
-    console.log(distanceTable)
+    const levenshteinDistance = getDistanceBetween(firstWord, secondWord)
+    console.log("The Levenshtein distance between " + firstWord + " and " + secondWord + " is " + levenshteinDistance)
 }
 
-const makeFirstRow = (firstWord) => {
-    let firstRow = [...Array(firstWord.length + 1).keys()]
-    return firstRow
-}
-
-const makeFirstCol = (secondWord) => {
-    let firstCol = []
-    for (let i = 0; i <= secondWord.length; i++)
-        firstCol.push([i])
-    return firstCol
-}
-
-const fillTable = (table, firstWord, secondWord) => {
-    cols = firstWord.length + 1
-    rows = secondWord.length + 1
-    for (let i = 1; i < rows; i++) {
-        for (let j = 1; j < cols; j++) {
-            table[i].push(999)
-        }
-    }
-
-    for (let i = 1; i < rows; i++) {
-        for (let j = 1; j < cols; j++) {
-            table[i][j] = getMinima(table[i], table[i-1], i, j, firstWord, secondWord)
-        }
-    }
-}
-
-const isSameLetter = (firstWord, secondWord, row, col) => {
-    return firstWord[col] === secondWord[row]
-}
-
-const getMinima = (currentLine, upperLine, row, col, firstWord, secondWord) => {
-    if (isSameLetter(firstWord, secondWord, row-1, col-1)) {
-        return upperLine[col-1]
-    }
-    const m1 = currentLine[col-1] + 1
-    const m2 = upperLine[col] + 1
-    const m3 = upperLine[col-1] + (isSameLetter(firstWord, secondWord, row-1, col-1) ? 0 : 2)
-    return Math.min(m1, m2, m3)
-}
-
-makeTable()
+main()
